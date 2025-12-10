227b0880673a08ad1dbdb3ed8c9cecbcbaaf6ef4
Mtdk.mitthu <mtdkmitthu@gmail.com>
2025-12-10 18:42:29 +0600

    Add Python Social Auth in  administration

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from account.models import Profile
 
 class EmailAuthBackend:
     """
@@ -19,3 +20,11 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+        
+
+
+    def create_profile(backend, user, response, *args, **kwargs):
+        """
+        Create a user profile for social authentication if it doesn't exist.
+        """
+        Profile.objects.get_or_create(user=user)
