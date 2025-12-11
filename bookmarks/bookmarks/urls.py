de3ee741b5a62fc8d27c058e30cdf98c5411799e
Mtdk.mitthu <mtdkmitthu@gmail.com>
2025-12-11 13:42:14 +0600

    Overriding the save() method of a ModelForm and image object savaing

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     # path('social-auth/',include('social_django.urls', namespace='social')),
     path('google-auth/',include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
 ]
 if settings.DEBUG:
     urlpatterns += static(
--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from .models import Image
+import requests
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -8,6 +13,7 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+
 
     def clean_url(self):
         url = self.cleaned_data['url']
@@ -18,3 +24,22 @@ class ImageCreateForm(forms.ModelForm):
                 'The given URL does not match valid image extensions.'
             )
         return url
+    
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        response = requests.get(image_url)
+        image.image.save(
+            image_name,
+            ContentFile(response.content),
+            save=False
+        )
+        
+        if commit:
+            image.save()
+        return image
