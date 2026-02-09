8f56e04c0471b1040c7b7a979af08118d8f7876f
Mtdk.mitthu <mtdkmitthu@gmail.com>
2026-02-09 23:55:06 +0600

    Creating many-to-many relationships with an intermediate model

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
+# Imports needed for the Signal (to make Profile working)
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(
@@ -15,3 +19,43 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile of {self.user.username}'
+
+class Contact(models.Model):
+    # The user who creates the relationship (The Follower)
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_from_set',
+        on_delete=models.CASCADE
+    )
+    # The user being followed (The Target)
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_to_set',
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+user_model = get_user_model()
+user_model.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Contact,
+        related_name='followers',
+        symmetrical=False
+    )
+)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
